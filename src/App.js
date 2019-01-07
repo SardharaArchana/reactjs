0d727ea543b91af6390d7144c305fb4cbe25f114
SardharaArchana <archana.sardhara@bacancytechnology.com>
2019-01-07 13:43:38 +0530

    make change in header button event

--- src/App.js
+++ src/App.js
@@ -4,12 +4,35 @@ import './App.css';
 import Header from './component/header'
 import Footer from './component/footer';
 import Body from './component/body/body';
+import SignUp from './component/body/signup';
+import Login from './component/body/login';
 
 class App extends Component {
+    constructor(props){
+      super();
+      this.state={
+        isClickRegister:false,
+        isClickLogin:false
+      }
+      this.register=this.register.bind(this);
+      this.login=this.login.bind(this);
+    }
+    register(rclick){
+      console.log('rclick',rclick);
+      this.state.isClickRegister=rclick;
+      console.log('rclc', this.state.isClickRegister);
+    };
+    login(lclick){
+      this.state.isClickLogin=lclick;
+    };
   render() {
+    
     return (   
+      
         <div style={{backgroundColor:'#1111'}}>
-            <div><Header/></div>
+            <div><Header onRegister={this.register} onLogin={this.login}/></div>            
+            {this.state.isClickRegister ? <div><SignUp/></div> : console.log('error') }
+            {this.state.isClickLogin ? <div><Login/></div> : console.log('error') }
             <div><Body /></div>
             <div><Footer/></div>
         </div>   
--- src/component/header.js
+++ src/component/header.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
+import SignUp from './body/signup';
+import login from './body/login';
 class Header extends Component {
+  constructor(props){
+    super(props);
+    this.onClickRegister=this.onClickRegister.bind(this);
+    this.onClickLogin=this.onClickLogin.bind(this);
+  }
+  onClickRegister(){
+      let rclick=true;
+      this.props.onRegister(rclick);
+  };
+  onClickLogin(){
+    let lclick=true;
+    this.props.onLogin(lclick);
+  };
   render() {
     const buttonStyle={ width:'100px', borderRadius:'12px', height:'30px', color:'grey', margin:'10px'};
     return ( 
       <div style={{height:'50px', backgroundColor:'#7878', padding:'10px'}}>
-            
-            <div style={{position:'fixed', right:'0px', marginRight:'10px'}}>
-            <a style={{fontSize:'30px',position:'fixed', left:'50px', padding:'10px'}}>REACT DEMO</a>
+            <a style={{fontSize:'30px',position:'absolute', left:'50px', padding:'10px'}}>REACT DEMO</a>
+            <div style={{position:'absolute', right:'0px', marginRight:'10px'}}>
               <button  style={buttonStyle}>home</button>
-              <button style={buttonStyle}>register</button>
-              <button style={buttonStyle}>login</button>                          
+              <button style={buttonStyle} onClick={this.onClickRegister}>register</button>
+              <button style={buttonStyle} onClick={this.onClickLogin}>login</button>                          
           </div> 
       </div>
     );
